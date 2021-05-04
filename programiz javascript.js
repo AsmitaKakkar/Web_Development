8c32ebc44e2884de681e7ab8d387fb080d45a69d
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-05 01:03:40 +0530

    --

--- programiz javascript.js
+++ programiz javascript.js
@@ -17,6 +17,24 @@ console.log(result);*/
 
 
 //to find square root of a number
-let a=16;
+/*let a=16;
 let result=Math.sqrt(a);
 console.log(result);
+*/
+
+
+//calculate the area of a triangle when base and height is known
+/*let base=12;
+let height=10;
+let result=(1/2)*base *height;
+console.log("the area of a triangle is : " +result);*/
+
+
+//calculate the area of a triangle when all the three sides are known
+let side1=10;
+let side2=20;
+let side3=20;
+let s=(side1+side2+side3)/2;
+
+let result=Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+console.log("Area of triangle is : "+result);
