28458ecfc80b0d98c88fef6582a28c9cd4f56c29
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-05 00:49:59 +0530

    --

---
+++ programiz javascript.js
@@ -0,0 +1,22 @@
+//print hello world to the console
+//console.log("Hello World");
+
+
+//adding two numbers 
+/*
+let a=10;
+let b=20;
+let result=a+b;
+console.log(result);*/
+
+
+//adding two numbers entered by the user itself
+//incomplete***************
+
+//let num1=prompt("Enter first number!!");
+
+
+//to find square root of a number
+let a=16;
+let result=Math.sqrt(a);
+console.log(result);
