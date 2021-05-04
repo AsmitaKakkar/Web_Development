d02e1c009dd465d76480fcdd090ff0e074b7c6d4
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-05 01:15:55 +0530

    --

--- programiz javascript.js
+++ programiz javascript.js
@@ -31,10 +31,33 @@ console.log("the area of a triangle is : " +result);*/
 
 
 //calculate the area of a triangle when all the three sides are known
-let side1=10;
+/*let side1=10;
 let side2=20;
 let side3=20;
 let s=(side1+side2+side3)/2;
 
 let result=Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
-console.log("Area of triangle is : "+result);
+console.log("Area of triangle is : "+result);*/
+
+
+//to swap two numbers
+/*let a=2;
+let b=3;
+let temp;
+console.log("Numbers before swapping :- "+ "a ="+ a+ " b = "+b);
+
+temp=a;
+a=b;
+b=temp;
+console.log("Numbers after swapping :- "+ "a = "+a +" b = "+b);
+*/
+
+
+//to swap two numbers using destructing assignment(es6)
+/*let a=10;
+let b=20;
+console.log("Numbers before swapping :- "+"a = "+a+" b = "+b);
+[a,b]=[b,a];
+console.log("Numbers after swapping :- "+"a = "+a+" b = "+b);*/
+
+//to swap two numbers using arithmatic operations
