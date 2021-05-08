8cf148e1fec6798be689e72baa6f596c59476618
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-05-08 23:45:51 +0530

    --

---
+++ presentation.js
@@ -0,0 +1,40 @@
+/*const p = "The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?";
+console.log(p.replace("dog", "monkey"));
+const regex = /Dog/ig;
+console.log(p.replace(regex, "ferret"));
+*/
+
+/*
+let S="hey!! This is asmita kakkar how are you from asmita!!";
+let reg=/asmita/g;
+let result =S.search(reg);
+console.log(result);
+result1 =S.search(reg);
+console.log(result1);
+
+*/
+
+
+
+/*let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+let r1=bigStr.match(bgRegex);
+let r2=bagStr.match(bgRegex);
+let r3=bugStr.match(bgRegex);
+let r4=bogStr.match(bgRegex);
+console.log(r1,r2,r3,r4);
+*/
+
+
+/*
+
+let A4 = "aaaah";
+      let A2 = "aah";
+      let multipleA = /a{5,3}h/;
+     let r1=multipleA.test(A4);
+     let r2=multipleA.test(A2);
+console.log(r1,r2);
+*/
