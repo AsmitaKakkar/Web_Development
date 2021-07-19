6947a5297d92897990203fd623e152b805d5b00d
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-07-20 04:46:38 +0530

    --

---
+++ app.js
@@ -0,0 +1,13 @@
+const {add,PI}= require('./math');
+
+console.log(add(2,3));
+
+console.log(PI);
+
+//console.log(math.square(9));
+
+const allCats=require('./cats_shelter');
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!!");
+
+console.log(allCats);
